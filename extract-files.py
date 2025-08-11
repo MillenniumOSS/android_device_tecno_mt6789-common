da756e6639053e4167a28de6777ba56a04c5d5d4
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-12 05:29:20 +0900

    mt6789-common: Stop patching mtk sensors subhal
    * Not needed anymore after https://github.com/Anything-at-25-00/android_device_tecno_mt6789-common/commit/3d20d1504de842f333d9b636bc957d99cef1f88b
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -58,7 +58,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
-    ('vendor/bin/mnld', 'vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so', 'vendor/lib64/mt6789/libaalservice.so', 'vendor/lib64/mt6789/libcam.utils.sensorprovider.so'): blob_fixup()
+    ('vendor/bin/mnld', 'vendor/lib64/mt6789/libaalservice.so', 'vendor/lib64/mt6789/libcam.utils.sensorprovider.so'): blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v32.so')
