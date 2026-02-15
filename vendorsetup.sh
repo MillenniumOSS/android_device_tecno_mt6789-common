021b914a14ff47648403e95b4ebe2a0f98977cc2
Shirayuki39 <lorddemecrius83@proton.me>
2026-02-15 19:48:40 +0800

    mt6789-common: vendorsetup: Add Aperture MediaTek EIS Mode patches and change patch remotes
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,20 +3,24 @@
 RET=0
 echo "- Applying fenrir compatiblity patches"
 cd system/core
-curl https://github.com/Nothing-2A/android_system_core/commit/8ff6e7a68523c3b870d8dcd5713c71ea15b43dd2.patch | git am || {
-  RET=$?
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/system/core/0001-libfs_avb-Allow-LKs-patched-with-fenrir-to-boot-on-A.patch | git am || {
+  RET=1
   git am --abort >/dev/null 2>&1
 }
-curl https://github.com/Nothing-2A/android_system_core/commit/0d5990a96c5e6a404887f5575c5d00bcbbaaef74.patch | git am || {
-  RET=$?
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/system/core/0002-fastbootd-Always-return-false-for-GetDeviceLockStatu.patch | git am || {
+  RET=1
   git am --abort >/dev/null 2>&1
 }
 cd ../../
 
-echo "- Applying Aperture Mediatek HFPS Mode Patch"
+echo "- Applying Aperture Mediatek HFPS Mode and EIS Patches"
 cd packages/apps/Aperture
-curl https://github.com/Nothing-2A/android_packages_apps_Aperture/commit/9509277efc852ad8bdcce204e0d9cfe104b6d190.patch | git am || {
-  RET=$?
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/packages/apps/Aperture/0001-Aperture-Enable-MediaTek-HFPS-Mode-for-60-FPS-video-.patch | git am || {
+  RET=1
+  git am --abort >/dev/null 2>&1
+}
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/packages/apps/Aperture/0002-Aperture-Enable-MediaTek-EIS-and-EIS-preview-mode-fo.patch | git am || {
+  RET=1
   git am --abort >/dev/null 2>&1
 }
 cd ../../../
