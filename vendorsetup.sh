c0c92da63e8812d50eb17af0a0d66096f16cb8dd
Shirayuki39 <lorddemecrius83@proton.me>
2026-02-19 19:52:19 +0800

    mt6789-common: vendorsetup: Change fenrir patch remote branch to `sixteen-yaap`
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,11 +3,11 @@
 RET=0
 echo "- Applying fenrir compatiblity patches"
 cd system/core
-curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/system/core/0001-libfs_avb-Allow-LKs-patched-with-fenrir-to-boot-on-A.patch | git am || {
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen-yaap/system/core/0001-libfs_avb-Allow-LKs-patched-with-fenrir-to-boot-on-A.patch | git am || {
   RET=1
   git am --abort >/dev/null 2>&1
 }
-curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/system/core/0002-fastbootd-Always-return-false-for-GetDeviceLockStatu.patch | git am || {
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen-yaap/system/core/0002-fastbootd-Always-return-false-for-GetDeviceLockStatu.patch | git am || {
   RET=1
   git am --abort >/dev/null 2>&1
 }
